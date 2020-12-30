ea8bf01efdf07e28342a9247de64184ab6e43b60
swaatipandey <swati.pandey@contentstack.com>
2020-12-30 12:39:31 +0530

    swati added backend and multer

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const router = require("./routes/employeeRoutes");
 const { config } = require("process");
-console.log();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
