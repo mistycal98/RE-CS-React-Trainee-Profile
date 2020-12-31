7dc11c4edb62d4f0ed8259cf06b4a73ccfe36dfd
venu-RAW <venu.sirpuram@raweng.com>
2020-12-31 13:56:40 +0530

    venu - did changes in styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,13 @@
-import {Component} from "react";
-import Profiles from "../components/Profiles"
+import { Component } from "react";
+import "../styles/Router.css";
 
-class Home extends Component{
-    render(){
-        return(
-        <div>
-                <h1>Home Page</h1>
-                <Profiles />
-        </div>
-        );
-    }
+class Home extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Home Page</h1>
+			</div>
+		);
+	}
 }
 export default Home;
